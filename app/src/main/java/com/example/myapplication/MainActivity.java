e7c707000583a027246dae405e51da3e44ab479a
upgamestudio <upgamestudio@gmail.com>
2025-04-09 16:31:20 +0300

    v0.0

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import static android.view.View.GONE;
+
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        EditText editText = findViewById(R.id.editTextTextPassword);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+            }
+        });
+    }
+}
